f337cbcd7fe216e0fc90dd6429c513c860684e31
KTM28 <56693434+KTM28@users.noreply.github.com>
2020-07-02 17:21:11 +0100

    comments added

--- app.py
+++ app.py
@@ -2,23 +2,26 @@ import os
 import pymysql
 from os import path
 
+# Get username and password from env.py
 if os.path.exists("env.py"):
     import env
 
 username = os.environ.get("username")
 password = os.environ.get("password")
-
+# Connect to the database
 connection = pymysql.connect(
     host="localhost", user=username, password=password, db="Chinook"
 )
 
 
 try:
+    # Run a Query
     with connection.cursor() as cursor:
         sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
+        cursor.execute(sql)  # execute that SQL command there and get result back
+        result = cursor.fetchall()  # Getting the data back
         print(result)
 
 finally:
+    # Close the connection, regardless of wheteher the above was successful
     connection.close()
